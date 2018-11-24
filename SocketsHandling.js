d4d677405dd3c4cde45de81b533ec0fade8abbad
Mehdi Abou <aboumehdi.pro@gmail.com>
2018-11-24 16:01:26 +0100

    Todo : Adding a shortest path feature

--- SocketsHandling.js
+++ SocketsHandling.js
@@ -1,6 +1,17 @@
+const LocationUtils = require('./utils/point_inside_polygon');
+
+
 exports = module.exports = function(http)
 {
+    const googleMapsClient = require('@google/maps').createClient({
+        key: 'AIzaSyA7e9hT7XciYcAf0g0lG4p0kYSGQBcrLrA'
+    });
+
     let io = require('socket.io')(http);
+    let send_location_interval;
+    let current_loc;
+    let polygon_area;
+    let center;
 
     function new_loc(current_position, meters_lat, meters_long)
     {
@@ -23,32 +34,87 @@ exports = module.exports = function(http)
     function PushMovements(current_position)
     {
         /* Position guide **  X, -X : TOP / X, X = RIGHT / -X, -X = LEFT / -X, X = BOT */
-        let value = 10;
-        let loc = new_loc(current_position, -value, value);
-        return loc;
+        let value = getRandomValue();
+        let value1 = getRandomValue();
+        current_loc = new_loc(current_position, value, value1);
+        return current_loc;
     }
+    function getRandomValue()
+    {
+        let random_number = (Math.random() * 20) - 10;
+        console.log(random_number);
+        return random_number;
 
+    }
     function SendFakeData(socket)
     {
         let begin_here = {lat: 48.8556475, lng: 2.2986304};
         socket.emit("send_begin_position", begin_here);
-        let counter = 0;
-        let new_loc = begin_here;
-        setInterval(function()
+        current_loc = begin_here;
+        send_location_interval =  setInterval(function()
         {
-            new_loc = PushMovements(new_loc);
-            socket.emit("movement", new_loc);
+            PushMovements(current_loc);
+            InsideOrOutsideChecker();
+            socket.emit("movement", current_loc);
         }, 2000) // Every 2 seconds
-
-
     }
 
 
+    //Todo
+    function calculate_shortest_path()
+    {
+        /*googleMapsClient.directions({
+            origin:,
+            destination:
+        })*/
+    }
+
+    function GoBackToCenter()
+    {
+        current_loc = center;
+    }
+
+    function StopMoving()
+    {
+        clearInterval(send_location_interval);
+    }
+
+
+    function InsideOrOutsideChecker()
+    {
+        const result = LocationUtils.robustPointInPolygon(polygon_area, [current_loc.lat, current_loc.lng]);
+        switch(result)
+        {
+            case -1:
+                console.log('inside');
+                break;
+            case 1:
+                console.log('outside');
+                GoBackToCenter();
+                break;
+            case 0:
+                console.log('on the limit');
+                break;
+        }
+    }
+
+    function getPolygonArea(area)
+    {
+        let top_left = [area[0].lat, area[0].lng];
+        let top_right = [area[1].lat, area[1].lng];
+        let bot_left = [area[2].lat, area[2].lng];
+        let bot_right = [area[3].lat, area[3].lng];
+
+        let bounds = [top_left, top_right, bot_left, bot_right];
+        return bounds;
+    }
+
     io.on('connection', function(socket)
     {
+
         console.log('a user connected');
         socket.emit('hello', "Hello Map!");
-        let center = {lat: 48.8556475, lng: 2.2986304};
+        center = {lat: 48.8556475, lng: 2.2986304};
         let area = [
                 {
                     lat:48.8569443, lng:2.2940138
@@ -66,6 +132,7 @@ exports = module.exports = function(http)
                     lat:48.8569443,lng:2.2940138
                 }
             ];
+        polygon_area =  getPolygonArea(area);
         let params = {
             center: center,
             area: area
@@ -73,6 +140,12 @@ exports = module.exports = function(http)
 
         socket.emit('get_map', params);
         SendFakeData(socket);
+
+        socket.on('stopmoving', function(data)
+        {
+            console.log('Stop moving');
+            StopMoving();
+        })
     });
 
 }
