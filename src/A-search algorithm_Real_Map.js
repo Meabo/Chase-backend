3deb52554029db0c519490d6938b055d1d6146e2
Mehdi Abou <aboumehdi.pro@gmail.com>
2018-12-08 01:07:03 +0100

    A* performance increase

--- src/A-search algorithm_Real_Map.js
+++ src/A-search algorithm_Real_Map.js
@@ -1,13 +1,22 @@
+//const LocationUtils = require('./utils/point_inside_polygon');
+const {performance} = require('perf_hooks');
+
+
+
 class AstarAlgorithm
 {
-    constructor(grid, row, col)
+    constructor(min_row, min_col, max_row, max_col)
     {
-        this.grid = grid;
-        this.row = row;
-        this.col = col;
+        //this.grid = grid;
+        this.min_row = min_row;
+        this.min_col = min_col;
+        this.max_row = max_row;
+        this.max_col = max_col;
+
         this.cell_details = [];
         this.closed_list = [];
         this.open_list = new Map();
+        this.result = [];
         this.destination_found = false;
     }
 
@@ -15,21 +24,19 @@ class AstarAlgorithm
     {
         //Is inside ? Need impl with real algo
         //
-        //console.log('Row and Col', row, col, this.row, this.col);
-        return (row >= 0) && (row < this.row) &&
-            (col >= 0) && (col < this.col);
+        //console.log('Row and Col', row, col, this.min_row, this.min_col, this.max_row, this.max_col);
+
+        //const result = LocationUtils.robustPointInPolygon(polygon_area, [current_loc.lat, current_loc.lng]);
+
+        return (row >= this.min_row) && (row < this.max_row) &&
+            (col >= this.min_col) && (col < this.max_col);
     }
 
     isUnBlocked(row, col)
     {
         // Returns true if the cell is not blocked else false
         //console.log(this.grid);
-       if (this.grid[row][col] === 1)
-       {
-           return true;
-       }
-       else
-           return false;
+       return true;
 
     }
 
@@ -41,10 +48,14 @@ class AstarAlgorithm
     calculateHeuristicValue(x, y, destination)
     {
         let hypothenus = Math.sqrt((x - destination.x)**2 + (y - destination.y) ** 2);
-        console.log('Hypothenus ', hypothenus);
+       // console.log('Hypothenus ', hypothenus);
         return hypothenus;
     }
 
+    get_final_result()
+    {
+        return this.result;
+    }
     initArray()
     {
         /*let cell_details = [];
@@ -59,23 +70,50 @@ class AstarAlgorithm
             }
 
         }*/
+        let matrix_test = [];
 
-        let matrix = new Array(this.row).fill(1).map(() => new Array(this.col).fill(new Cell(-1, -1, Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)));
-        return matrix;
+        for (let i = this.min_row; i < this.max_row; i++)
+        {
+            matrix_test[i] = [];
+
+            for (let j = this.min_col; j < this.max_col; j++)
+            {
+                matrix_test[i][j] = new Cell(-1, -1, Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER);
+            }
+        }
+
+       /* let matrix = new Array(this.max_row).fill(1).map(() => new Array(this.max_col).fill(new Cell(-1, -1, Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)));
+        return matrix;*/
+
+       return matrix_test;
     }
 
 
     initArrayClosed()
     {
-        let matrix = new Array(this.row).fill(false).map(() => new Array(this.col).fill(false));
-        return matrix;
+
+
+        let matrix_test = [];
+
+        for (let i = this.min_row; i < this.max_row; i++)
+        {
+            matrix_test[i] = [];
+
+            for (let j = this.min_col; j < this.max_col; j++)
+            {
+                matrix_test[i][j] = false;
+            }
+        }
+
+        //let matrix = new Array(this.max_row).fill(false).map(() => new Array(this.max_col).fill(false));
+        return matrix_test;
     }
 
-     update_neightbor(current_x, current_y, prev_x, prev_y, src, dest, value)
+    async update_neightbor(current_x, current_y, prev_x, prev_y, src, dest, value)
     {
         if (this.isValid(current_x, current_y) === true)
         {
-            console.log('Valid Cell', current_x, current_y, dest);
+            //console.log('Valid Cell', current_x, current_y, dest);
             if (this.isDestination(current_x, current_y, dest))
             {
                 // We found the destination
@@ -85,8 +123,8 @@ class AstarAlgorithm
                 this.cell_details[current_x][current_y].set_parent_x(prev_x);
                 this.cell_details[current_x][current_y].set_parent_y(prev_y);
                 this.destination_found = true;
-                this.displaylist(this.cell_details);
-                this.trace_path(src, dest);
+               // this.displaylist(this.cell_details);
+                //this.trace_path(src, dest);
                 return true;
             }
             else if (this.closed_list[current_x][current_y] === false && this.isUnBlocked(current_x, current_y) === true)
@@ -98,18 +136,18 @@ class AstarAlgorithm
                 if ((this.cell_details[current_x][current_y]).get_f() === Number.MAX_SAFE_INTEGER ||
                     (this.cell_details[current_x][current_y]).get_f() > new_f)
                 {
-                    console.log('Entered here');
+                    //console.log('Entered here');
                     // We add the list in the open_list
                     this.open_list.set([current_x, current_y], new_f);
                     // We update the cell
-                    this.cell_details[current_x][current_y] = new Cell(prev_x, prev_y, new_g, new_h, new_f);
+                    (this.cell_details[current_x][current_y]).updateCell(prev_x, prev_y, new_g, new_h, new_f);
                 }
             }
         }
         else
         {
-            console.log('Not valid cell');
-            console.log("\n");
+            //console.log('Not valid cell');
+            //console.log("\n");
             return false;
         }
         return false;
@@ -138,74 +176,67 @@ class AstarAlgorithm
 
         //console.log(x, y)
         // We push the source
-
+        this.result = [...queue];
         while (queue.length > 0)
         {
             let position = queue.pop();
             console.log("-> ", position[0], position[1]);
         }
+        //console.log('finished');
     }
 
-    visit_neighbors(x, y, src, dest)
+    async visit_neighbors(x, y, src, dest)
     {
         let result = false;
 
-        console.log('Visiting Neighbors', x, y);
-
-        Promise.all([this.update_neightbor(x - 1, y, x, y, src, dest, 1.0),
-            this.update_neightbor(x + 1, y, x, y, src, dest, 1.0),
-            this.update_neightbor(x, y - 1, x, y, src, dest, 1.0),
-            this.update_neightbor(x, y + 1, x, y, src, dest, 1.0),
-            this.update_neightbor(x - 1, y + 1, x, y, src, dest, Math.sqrt(2)),
-            this.update_neightbor(x + 1, y + 1, x, y, src, dest, Math.sqrt(2)),
-            this.update_neightbor(x - 1, y - 1, x, y, src, dest, Math.sqrt(2)),
-            this.update_neightbor(x + 1, y - 1, x, y, src, dest, Math.sqrt(2))]).then(function(result)
-        {
-            console.log('Result', result);
-        })
-
-
+        //console.log('Visiting Neighbors', x, y);
         // Four directions (North, South, East, West)
-       /* result =  this.update_neightbor(x - 1, y, x, y, src, dest, 1.0);
+
+
+
+
+        result =  await this.update_neightbor(x - 1, y, x, y, src, dest, 1.0);
         if (result === true)
             return true;
 
-        result =  this.update_neightbor(x + 1, y, x, y, src, dest, 1.0);
+        result =  await this.update_neightbor(x + 1, y, x, y, src, dest, 1.0);
         if (result === true)
             return true;
 
-        result =  this.update_neightbor(x, y - 1, x, y, src, dest, 1.0);
+        result = await this.update_neightbor(x, y - 1, x, y, src, dest, 1.0);
         if (result === true)
             return true;
 
-        result =  this.update_neightbor(x, y + 1, x, y, src, dest, 1.0);
+        result = await this.update_neightbor(x, y + 1, x, y, src, dest, 1.0);
         if (result === true)
             return true;
 
         // Diagonals
-        result =   this.update_neightbor(x - 1, y + 1, x, y, src, dest, Math.sqrt(2));
+        result =  await this.update_neightbor(x - 1, y + 1, x, y, src, dest, Math.sqrt(2));
         if (result === true)
             return true;
 
-        result =   this.update_neightbor(x + 1, y + 1, x, y, src, dest, Math.sqrt(2));
+        result = await this.update_neightbor(x + 1, y + 1, x, y, src, dest, Math.sqrt(2));
         if (result === true)
             return true;
 
-        result =   this.update_neightbor(x - 1, y - 1, x, y, src, dest, Math.sqrt(2));
+        result =  await this.update_neightbor(x - 1, y - 1, x, y, src, dest, Math.sqrt(2));
         if (result === true)
             return true;
 
-        result =   this.update_neightbor(x + 1, y - 1, x, y, src, dest, Math.sqrt(2));
+        result =  await this.update_neightbor(x + 1, y - 1, x, y, src, dest, Math.sqrt(2));
         if (result === true)
-            return true; */
+            return true;
+
+
 
     }
 
     displaylist(array_)
     {
-        for (let i = 0; i < this.row; i++)
+        for (let i = this.min_row; i < this.max_row; i++)
         {
-            for (let j = 0; j < this.col; j++)
+            for (let j = this.min_col; j < this.max_col; j++)
             {
                 console.log('Cell Details', i, j, array_[i][j]);
             }
@@ -244,7 +275,7 @@ class AstarAlgorithm
     }
 
 
-     AstarSearch(source, destination)
+     async AstarSearch(source, destination)
     {
         //console.log(this.isUnBlocked(destination.x, destination.y));
         if (this.require_checking(source, destination) === false)
@@ -264,7 +295,7 @@ class AstarAlgorithm
         while (this.open_list.size > 0)
         {
             let first_entry = this.open_list.entries().next();
-            console.log('Current Open List', this.open_list.entries());
+            //console.log('Current Open List', this.open_list.entries());
             this.open_list.delete(first_entry.value[0]);
 
             let first_entry_values = first_entry.value[0];
@@ -273,12 +304,12 @@ class AstarAlgorithm
             this.closed_list[first_entry_x][first_entry_y] = true;
 
             // Now, we need to check 8 next neighbors
-           let result =  this.visit_neighbors(first_entry_x, first_entry_y, source, destination);
+           let result =  await this.visit_neighbors(first_entry_x, first_entry_y, source, destination);
            if (result === true)
            {
 
                //console.log('Destination Found');
-              // this.displaylist(this.cell_details);
+               //this.displaylist(this.cell_details);
 
                return true;
            }
@@ -300,6 +331,15 @@ class Cell
     constructor(parent_x, parent_y, g, h, f) {
         this.parent_x = parent_x;
         this.parent_y = parent_y;
+        this.g = g;
+        this.h = h;
+        this.f = f;
+    }
+
+    updateCell(prev_x, prev_y, g, h, f)
+    {
+        this.parent_x = prev_x;
+        this.parent_y = prev_y;
         this.g = g;
         this.h = h;
         this.f = f;
@@ -338,23 +378,123 @@ class Cell
 }
 
 
-
-function Astar()
+function getPolygonArea(area)
 {
-    let src = {x:0, y:0};
-    let dest = {x: 4, y:4};
+    let top_left = [area[0].lat, area[0].lng];
+    let top_right = [area[1].lat, area[1].lng];
+    let bot_left = [area[2].lat, area[2].lng];
+    let bot_right = [area[3].lat, area[3].lng];
 
-    let grid = [
+    let bounds = [CoordinatesXY(top_left[0], top_left[1]),
+        CoordinatesXY(top_right[0], top_right[1]),
+        CoordinatesXY(bot_left[0], bot_left[1]),
+        CoordinatesXY(bot_right[0], bot_right[1])];
+
+   /* console.log(CoordinatesXY(top_left[0], top_left[1]));
+    console.log(CoordinatesXY(top_right[0], top_right[1]));
+    console.log(CoordinatesXY(bot_left[0], bot_left[1]));
+    console.log(CoordinatesXY(bot_right[0], bot_right[1]));*/
+    return bounds;
+}
+
+
+
+function CoordinatesXY(lat, lng)
+{
+    let factor = 25;
+    let TILE_SIZE = 256;
+    let scale = 1 << 20;
+
+    let siny = Math.sin(lat * Math.PI / 180);
+    siny = Math.min(Math.max(siny, -0.9999), 0.9999);
+
+    let x = TILE_SIZE * (0.5 + lng / 360);
+    let y = TILE_SIZE * (0.5 - Math.log((1 + siny) / (1 - siny)) / (4 * Math.PI));
+
+
+    let tile_x = Math.floor(x * scale / factor);
+    let tile_y = Math.floor(y * scale / factor);
+
+    return [tile_x, tile_y];
+}
+
+let area = [
+    {
+        //trocadero
+        lat:48.8631503, lng:2.2864126
+    },
+    {
+        //alma marceau
+        lat:48.8646175, lng:2.3010568
+    },
+    {
+        //Emile Zola
+        lat:48.8469874, lng:2.2951146
+    },
+    {
+        //Sevres Lecourbe
+        lat:48.8456454, lng:2.3092419
+    }
+];
+
+async function Astar()
+{
+
+    let center = {lat: 48.857206, lng: 2.2946687};
+    let coordinates_src = CoordinatesXY(center.lat, center.lng);
+    let random_point_inside = {lat:48.8501335, lng:2.2996423};
+    let coordinates_dest = CoordinatesXY(random_point_inside.lat, random_point_inside.lng);
+
+    let src = {x: coordinates_src[0], y:coordinates_src[1]};
+    let dest = {x: coordinates_dest[0], y:coordinates_dest[1]};
+    let bounds = getPolygonArea(area);
+
+
+   console.log('Source', src);
+    console.log('Destination', dest);
+    console.log('Bounds', bounds);
+
+   /* let grid = [
         [1, 1, 0, 1, 0],
         [1, 0, 1, 1, 0],
         [1, 0, 0, 1, 0],
         [1, 1, 1, 0, 1],
         [1, 1, 1, 0, 1],
-    ];
+    ];*/
 
-    let astar = new AstarAlgorithm(grid, 5, 5);
-    console.log(astar.grid);
-    astar.AstarSearch(src, dest);
+    let astar = new AstarAlgorithm(bounds[0][0], bounds[1][1], bounds[3][0], bounds[3][1]);
+    //console.log(astar.grid);
+    let t0 = performance.now();
+    await astar.AstarSearch(src, dest);
+    console.log(astar.trace_path(src, dest))
+    console.log('Number Steps', astar.get_final_result().length);
+    let t1 = performance.now();
+    console.log("Perfomance : A* Algorithm took " + (t1 - t0) + " milliseconds.");
+
+    //original_coordinates();
 }
+
+function original_coordinates(result)
+{
+    console.log(result);
+    let original = [];
+    for (let i = 0; i < result.length; i++)
+    {
+        //console.log('Result', result[i][0]);
+        let values = result[i];
+        let val_x = values[0] + 530000;
+        let val_y = values[1] + 360000;
+        original.push([val_x, val_y]);
+    }
+    console.log(original);
+}
+
+function modulo_10(value)
+{
+
+    console.log(value%1000);
+}
+
+//modulo_10(530983)
 
 Astar();
